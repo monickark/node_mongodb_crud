a6937460401d6cb4148e9f5623ba8a86d5dbdd56
Monicka Akilan <monicka.rk@gmail.com>
2023-06-12 16:59:22 +0530

    added router

--- routes.js
+++ routes.js
@@ -1,33 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express")
+const {Dog} = require("./models") 
+const router = express.Router()
 
-const { Dog } = require("./models");
-const routes = require("./routes")
-
-const app = express();
-
-app.use(express.json());
-app.use("/api", routes)
-
-app.get("/dogs", async (req, res) => {
+// Get all dogs
+router.get("/dogs", async (req, res) => {
   const allDogs = await Dog.find();
   return res.status(200).json(allDogs);
 });
 
-app.get("/dogs/:id", async (req, res) => {
+router.get("/dogs/:id", async (req, res) => {
   const { id } = req.params;
   const dog = await Dog.findById(id);
   return res.status(200).json(dog);
 });
 
-app.post("/dogs", async (req, res) => {
-    console.log("request body: "+JSON.stringify(req.body));
+router.post("/dogs", async (req, res) => {
+  console.log("request body: "+JSON.stringify(req.body));
   const newDog = new Dog({ ...req.body });
   const insertedDog = await newDog.save();
   return res.status(201).json(insertedDog);
 });
 
-app.put("/dogs/:_id", async (req, res) => {
+router.put("/dogs/:_id", async (req, res) => {
   try {
     const { _id } = req.params;  
     console.log("request body: "+JSON.stringify(req.body));
@@ -43,22 +37,10 @@ app.put("/dogs/:_id", async (req, res) => {
   }
 });
 
-app.delete("/dogs/:id", async (req, res) => {
+router.delete("/dogs/:id", async (req, res) => {
   const { id } = req.params;
   const deletedDog = await Dog.findByIdAndDelete(id);
   return res.status(200).json(deletedDog);
 });
 
-const start = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://monickakilan:5xpeNSAGeOOhYUzN@dogschema.4ocrfkv.mongodb.net/?retryWrites=true&w=majority"
-    );
-    app.listen(3000, () => console.log("Server started on port 3000"));
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-start();
+module.exports = router
